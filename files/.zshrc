4a62e02410115f6582943a115737df38c5d3c136
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-07-10 18:19:58 +0530

    Add files via upload

---
+++ files/.zshrc
@@ -0,0 +1,14 @@
+clear
+source ~/Termux-zsh-theme\ installed/theme.sh
+source ~/.oh-my-zsh/oh-my-zsh.sh
+source ~/Termux-zsh-theme\ installed/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/Termux-zsh-theme\ installed/zsh-autosuggestions/zsh-autosuggestions.zsh
+printf "\n\033[1;93m====================================================================\n"
+printf "\t\t\t \033[0mTermux-zsh-theme"
+printf "\n\033[1;93m====================================================================\n"
+source ~/Termux-zsh-theme\ installed/checking.sh
+alias a='apt install'
+alias p='python'
+alias p2='python2'
+alias p3='python3'
+alias g='git clone'
--- files/checking.sh
+++ files/checking.sh
@@ -1,1 +1,55 @@
+gitp() {
+	cd $PREFIX/bin
+	if [ -e git ];then
+	ha
+	else
+	na
+	fi
+	}
+	phph() {
+	fo
+	if [ -e php ];then
+	ha
+	else
+	na
+	fi
+	}
+pythonn() {
+	fo
+	if [ -e python ];then
+	ha
+	else
+	na
+	fi
+	}
+	wgett() {
+	fo
+	if [ -e wget ];then
+	ha
+	else
+	na
+	fi
+	}
 
+
+	openssh() {
+	fo
+	if [ -e ssh ];then
+	ha
+	else
+	na
+	fi
+	}
+	
+	git="$(gitp)"
+ssh="$(openssh)"
+php="$(phph)"
+wget="$(wgett)"
+python="$(pythonn)"
+printf "\033[1;92m Checking git >> $git\n"
+printf "\033[1;92m Checking openssh >> $ssh\n"
+printf "\033[1;92m Checking php >> $php\n"
+printf "\033[1;92m Checking wget >> $wget\n"
+printf "\033[1;92m Checking python >> $python\n"
+echo
+cd ~
